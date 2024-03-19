f0f66f676d6fb0f018d8645fec7628a01cacfa72
arnaldojuniorads@gmail.com <weslley_ss@live.com>
2024-03-18 21:44:17 -0300

    alterações Lista de produtos

--- AplicacaoWeb/Backend/Program.cs
+++ AplicacaoWeb/Backend/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Produto> produtos = new List<Produto>()
+{
+    new Produto("Long Sword", "Warrior/Two Handed"),
+    new Produto("Lethal Knife", "Assasin/Rogue/Bandit"),
+    new Produto("Battle Gloves", "Pugilist"),
+    new Produto("Elemental Staff", "Mage")
+
+};
+
+//End points = Funcionalidades
+app.MapPost("/api/produto", () => "Primeira API!");
+
+app.MapGet("/api/produto", () => produtos);
+
+
 
 app.Run();
+
+public record Produto(string Nome, string Descricao);
